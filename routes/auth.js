0549230d0f42b461b4026015de92c9a299b66f96
Rumi Rifai <rumirifai96@gmail.com>
2024-12-10 17:31:15 +0700

    Fixing req body method

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,6 @@
-const express = require('express');
 const User = require('../models/User');
+const express = require('express');
 const jwt = require('jsonwebtoken');
-
 const router = express.Router();
 
 router.post('/register', async (req, res) => {
@@ -16,7 +15,6 @@ router.post('/register', async (req, res) => {
         const user = await User.create({ name, email, password });
         res.status(201).json({ message: 'User registered successfully', user });
     } catch (err) {
-        console.log(err);
         res.status(500).json({ error: err.message });
     }
 });
