81421391a7e487e98b97e4afa02a1fb2efa2931d
Rumi Rifai <rumirifai96@gmail.com>
2024-12-11 23:36:17 +0700

    Adding some feature in routes

--- routes/trees.js
+++ routes/trees.js
@@ -31,13 +31,15 @@ router.post('/result', authMiddleware, upload.single('file'), async (req, res) =
         const disease_id = modelResponse.data.prediction;
         const predictResult = await Trees.disease({disease_id});
 
-        const { predict, details } = predictResult;    
+        const { predict, details, indication, treatment } = predictResult;    
         const image_url = modelResponse.data.file_url;
         const status = predict;
         const user_id = req.user.id;
 
-        const saved = await Trees.saved({ status, details, image_url, user_id, disease_id })
-        return res.status(201).json({ message: "Here's the analyze result", predictResult, saved });
+        const saved = await Trees.saved({ status, details, indication, treatment, image_url, user_id, disease_id })
+        if (saved) {
+            return res.status(201).json({ message: "Here's the analyze result. Analyze Result has been saved to your history", predictResult});
+        }
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
@@ -57,4 +59,16 @@ router.get('/history', authMiddleware, async (req, res) => {
     }
 });
 
+router.post('/historyById', authMiddleware, async (req, res) => {
+    const scan_id = req.body.scan_id;
+    try {
+        const historyId = await Trees.historyById({ scan_id });
+        if (!historyId) {
+            return res.status(404).json({ message: "History not found "});
+        }
+        res.status(201).json({ message: historyId });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+})
 module.exports = router;
